a3de77b114e5c7f2288ec4e0e1d4de5889449a7e
liuguilin <847072596@qq.com>
2016-04-18 10:51:22 +0800

    新增功能

--- src/com/my/app/MainActivity.java
+++ src/com/my/app/MainActivity.java
@@ -4,6 +4,7 @@ import com.example.testgit.R;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 	
@@ -11,6 +12,9 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main_activity);
+		
+		Toast.makeText(this, "我的项目", Toast.LENGTH_SHORT).show();
+		
 	}
 
 }

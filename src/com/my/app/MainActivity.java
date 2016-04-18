8ffb6980e02b1f42df5bea21b89b2f8416568cf7
liuguilin <847072596@qq.com>
2016-04-18 10:27:51 +0800

    init project

---
+++ src/com/my/app/MainActivity.java
@@ -0,0 +1,16 @@
+package com.my.app;
+
+import com.example.testgit.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main_activity);
+	}
+
+}
